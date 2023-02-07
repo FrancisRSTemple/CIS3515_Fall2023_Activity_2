aad24e9582101352af1064996509ce25db5021e2
Francis R Scallatino <tup31915@temple.edu>
2023-02-07 15:13:19 -0500

    last step completed

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +25,14 @@ class MainActivity : AppCompatActivity() {
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                p0?.run{
+                    displayTextView.textSize = getItemAtPosition(p2).toString().toFloat()
+                }
+            }
 
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
     }
 }
